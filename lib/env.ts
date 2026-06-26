7817070a5987abc14a04df4bb336434000c8eca5
sonniecagnazzi <sonnie.cagnazzi@gmail.com>
2026-06-26 23:04:28 +0200

    Update env.ts

--- lib/env.ts
+++ lib/env.ts
@@ -6,10 +6,24 @@ function required(name: string): string {
 
 export const env = {
   supabaseUrl: () => required('SUPABASE_URL'),
+
   supabaseServiceRoleKey: () => required('SUPABASE_SERVICE_ROLE_KEY'),
-  resendApiKey: () => process.env.RESEND_API_KEY || '',
-  alertFromEmail: () => process.env.ALERT_FROM_EMAIL || 'Music Price Monitor <onboarding@resend.dev>',
+
+  smtpHost: () => required('SMTP_HOST'),
+
+  smtpPort: () => Number(process.env.SMTP_PORT || '465'),
+
+  smtpUser: () => required('SMTP_USER'),
+
+  smtpPassword: () => required('SMTP_PASSWORD'),
+
+  alertFromEmail: () =>
+    process.env.ALERT_FROM_EMAIL ||
+    `Music Price Monitor <${required('SMTP_USER')}>`,
+
   defaultAlertEmail: () => process.env.DEFAULT_ALERT_EMAIL || '',
+
   cronSecret: () => required('CRON_SECRET'),
+
   appBaseUrl: () => process.env.APP_BASE_URL || ''
 };
